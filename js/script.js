cf96e39396e3d8124595aec981a1c507c1c10e5b
DzmitryMiankou <gmiankou@gmail.com>
2022-08-12 14:40:29 +0300

    ffg

--- js/script.js
+++ js/script.js
@@ -392,21 +392,6 @@ var tl = anime({
 
 
 
-function isElementInViewport(el, inCB, outCB, rootMargin) {
-  var margin = rootMargin || '-10%';
-  function handleIntersect(entries, observer) {
-    var entry = entries[0];
-    if (entry.isIntersecting) {
-      if (inCB && typeof inCB === 'function') inCB(el, entry);
-    } else {
-      if (outCB && typeof outCB === 'function') outCB(el, entry);
-    }
-  }
-  var observer = new IntersectionObserver(handleIntersect, {rootMargin: margin});
-  observer.observe(el);
-}
-
-
 
 
 
