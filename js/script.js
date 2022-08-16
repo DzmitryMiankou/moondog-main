48d29111791203e73a04b8961b6dfe1812b5e0e2
DzmitryMiankou <gmiankou@gmail.com>
2022-08-16 15:43:37 +0300

    kl

--- js/script.js
+++ js/script.js
@@ -582,6 +582,51 @@ function fixedPositionTextAmim(arr, target) {
 
 
 
+
+
+
+
+
+
+import Letterize from "https://cdn.skypack.dev/letterizejs@2.0.0";
+const test = new Letterize({
+        targets: ".animate-me"
+      });
+
+      const animation = anime.timeline({
+        targets: test.listAll,
+        delay: anime.stagger(100, {
+          grid: [test.list[0].length, test.list.length],
+          from: "center"
+        }),
+        loop: true
+      });
+
+      animation
+        .add({
+          scale: 0.5
+        })
+        .add({
+          letterSpacing: "10px"
+        })
+        .add({
+          scale: 2
+        })
+        .add({
+          letterSpacing: "6px"
+        });
+
+
+
+
+
+
+
+
+
+
+
+
 /*Функция проверки наличия объекта в зоне видимости */
 function isElementInViewport(el, inCB, outCB, rootMargin) {
   const margin = rootMargin || '-10%';
